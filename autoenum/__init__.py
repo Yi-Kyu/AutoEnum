b160b20f49ffecff29b97de98b1b63d5fc263145
Yi-Kyu <yikyuontw@gmail.com>
2025-03-20 03:16:35 -0600

    Update __init__.py

---
+++ autoenum/__init__.py
@@ -0,0 +1,10 @@
+"""
+AutoEnum - Framework modular de escaneo y enumeración para pruebas de penetración
+"""
+
+__version__ = "1.0.0"
+__author__ = "AutoEnum Team"
+__description__ = "Framework modular de escaneo y enumeración para pruebas de penetración"
+
+# Exportar clases/funciones principales para facilitar su importación
+from autoenum.framework.core import AutoEnumFramework
