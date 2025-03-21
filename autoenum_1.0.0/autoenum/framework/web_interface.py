b974b1f170c115806f452a099751b661f58dd910
Yi-Kyu <yikyuontw@gmail.com>
2025-03-20 20:59:28 -0600

    web_interface.py

--- autoenum_1.0.0/autoenum/framework/web_interface.py
+++ autoenum_1.0.0/autoenum/framework/web_interface.py
@@ -10,7 +10,7 @@ import json
 import logging
 import threading
 import webbrowser
-from autoenum.framework.core import AutoEnumFramework
+from core import AutoEnumFramework
 from datetime import datetime
 from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
 from werkzeug.utils import secure_filename
